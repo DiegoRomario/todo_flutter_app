da14b2bd7fc0169b56992171f3eae41012b8898a
Diego Romario <diego.romario@outlook.com>
2020-02-22 13:47:09 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("What do Ir have to do?"),
+        ),
+        body: Container(
+          child: Center(child: Text("Demo")),
+        ));
+  }
+}
