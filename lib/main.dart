4802715c2edfc832a955225fd01150a699ef2922
Diego Romario <diego.romario@outlook.com>
2020-02-22 15:33:55 -0300

    adding new features

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models/item.dart';
 
 void main() => runApp(App());
 
@@ -9,22 +14,124 @@ class App extends StatelessWidget {
       title: 'Todo App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.indigo,
       ),
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  HomePage() {
+    items = [];
+    // items.add(Item(done: true, title: "work"));
+    // items.add(Item(done: false, title: "soccer"));
+    // items.add(Item(done: true, title: "books"));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState() {
+    load();
+  }
+
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    if (newTaskCtrl.text.isNotEmpty) {
+      setState(() {
+        widget.items.add(
+          Item(done: false, title: newTaskCtrl.text),
+        );
+        newTaskCtrl.clear();
+        save();
+      });
+    }
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      save();
+    });
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("What do Ir have to do?"),
+      appBar: AppBar(
+          title: TextFormField(
+        controller: newTaskCtrl,
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Colors.lightBlue,
+          fontSize: 18,
         ),
-        body: Container(
-          child: Center(child: Text("Demo")),
-        ));
+        decoration: InputDecoration(
+            labelText: "New item",
+            labelStyle: TextStyle(color: Colors.white, fontSize: 24)),
+      )),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index) {
+          final item = widget.items[index];
+          return Dismissible(
+            child: CheckboxListTile(
+              title: Text(item.title),
+              key: Key(item.title),
+              value: item.done,
+              onChanged: (value) {
+                setState(() {
+                  item.done = value;
+                  save();
+                });
+              },
+            ),
+            key: Key(item.title),
+            background: Container(
+              color: Colors.red.withOpacity(0.4),
+              child: new Padding(
+                padding: new EdgeInsets.all(13),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Icon(Icons.delete),
+                ),
+              ),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
+      ),
+    );
   }
 }
